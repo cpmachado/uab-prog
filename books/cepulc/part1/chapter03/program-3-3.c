360bbdfd38baa187e6c2fa095ef7e477c489339f
Carlos Pinto Machado <cpmachado@protonmail.com>
2022-09-25 14:06:16 +0100

    program-3-3

---
+++ books/cepulc/part1/chapter03/program-3-3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+main(void) {
+    int ano, mes;
+
+    printf("Indique o ano: ");
+    scanf(" %d", &ano);
+
+    printf("Indique o mes: ");
+    scanf(" %d", &mes);
+
+    if (mes == 2) {
+        if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+            printf("29\n");
+        } else {
+            printf("28\n");
+        }
+    } else if (mes == 1 || mes == 3 || mes == 5 || mes == 7
+            || mes == 8 || mes == 10 || mes == 12) {
+        printf("31\n");
+    } else {
+        printf("30\n");
+    }
+
+    return 0;
+}
