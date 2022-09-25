9cbd514b4c6e32dbae9efde290c2d2f0eb5a0784
C. Augusto Pinto Machado <40689339+cpmachado@users.noreply.github.com>
2022-09-25 14:46:29 +0100

    Cepulc: chapter 4 (#5)
    * program-4-1
    
    * program-4-2
    
    * program-4-3
    
    * program-4-4

---
+++ books/cepulc/part1/chapter04/program-4-4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+main(void) {
+    int a, b, k, contagem;
+
+    printf("Indique K: ");
+    scanf(" %d", &k);
+
+    contagem = 0;
+    a = 1;
+
+    while (a <= k) {
+        b = 1;
+        while (b <= k) {
+            if ( a + b <= k && a * b <= k) {
+                contagem++;
+            }
+            b++;
+        }
+        a++;
+    }
+
+    printf("Total: %d\n", contagem);
+
+    return 0;
+}
