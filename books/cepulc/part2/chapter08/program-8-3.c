06c46f33ae93e2d8a6f846a562a04463e71ab970
cpmachado <cpmachado@protonmail.com>
2022-10-23 21:51:28 +0100

    program-8-3

---
+++ books/cepulc/part2/chapter08/program-8-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+void
+troca(int* x, int* y) {
+    int aux = *x;
+    *x = *y;
+    *y = aux;
+}
+
+int
+main(void) {
+    int x = 123, y = 321;
+
+    troca(&x, &y);
+    printf("x: %d y: %d\n", x, y);
+
+    return 0;
+}
