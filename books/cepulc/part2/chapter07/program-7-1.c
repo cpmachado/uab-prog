0ebb09ba1ac63dd13c1dbf0734b772a92cd5a468
cpmachado <cpmachado@protonmail.com>
2022-10-23 17:31:42 +0100

    program-7-1

---
+++ books/cepulc/part2/chapter07/program-7-1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int
+main(void) {
+    int segunda, terca, quarta, quinta, sexta, sabado, domingo;
+    int total;
+
+    printf("Segunda: ");
+    scanf(" %d", &segunda);
+    printf("Terca: ");
+    scanf(" %d", &terca);
+    printf("Quarta: ");
+    scanf(" %d", &quarta);
+    printf("Quinta: ");
+    scanf(" %d", &quinta);
+    printf("Sexta: ");
+    scanf(" %d", &sexta);
+    printf("Sabado: ");
+    scanf(" %d", &sabado);
+    printf("Domingo: ");
+    scanf(" %d", &domingo);
+
+    total = segunda + terca + quarta + quinta + sexta + sabado + domingo;
+    printf("Soma: %d\n", total);
+    printf("Media: %f\n", total/7.0);
+
+    return 0;
+}
+
