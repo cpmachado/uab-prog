dc5e69133f10112aff49a83fd989ca33d0054eef
cpmachado <cpmachado@protonmail.com>
2022-10-23 16:56:12 +0100

    program-6-6

---
+++ books/cepulc/part2/chapter06/program-6-6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int
+main(void) {
+    int opcao;
+
+    do {
+        printf("Menu:\n");
+        printf("1 - opcao A\n");
+        printf("2 - opcao B\n");
+        printf("3 - opcao C\n");
+        printf("0 - sair\n");
+        printf("Opcao: ");
+        scanf(" %d", &opcao);
+
+        if (opcao == 1) {
+            printf("Opcao escolhida A\n");
+        } else if (opcao == 2) {
+            printf("Opcao escolhida B\n");
+        } else if (opcao == 3) {
+            printf("Opcao escolhida C\n");
+        } else {
+            printf("Opcao invalida\n");
+        }
+    } while (opcao > 0);
+
+    printf("Fim do programa.\n");
+
+    return 0;
+}
