b1901d7f891603006086a685bbb3fa60a39ced19
cpmachado <cpmachado@protonmail.com>
2022-10-23 22:24:59 +0100

    program-9-2

---
+++ books/cepulc/part2/chapter09/program-9-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+void
+hanoi(int de, int para, int medio, int discos) {
+    static char pilhas[3][9] = {
+        "esquerda",
+        "meio",
+        "direita"
+    };
+
+    if (discos == 1) {
+        printf("Mover do(a) %s para %s\n", pilhas[de], pilhas[para]);
+    }
+    if (discos > 1) {
+        hanoi(de, medio, para, discos - 1);
+        hanoi(de, para, medio, 1);
+        hanoi(medio, para, de, discos - 1);
+    }
+}
+
+int
+main(void) {
+    int discos;
+
+    printf("Torres de Hanoi. Quantos discos quer movimentar? ");
+    scanf(" %d", &discos);
+    hanoi(0, 2, 1, discos);
+    return 0;
+}
