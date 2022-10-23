526e629930c61a42c1f649531be1e301792af2d0
cpmachado <cpmachado@protonmail.com>
2022-10-23 17:05:11 +0100

    program-6-8

---
+++ books/cepulc/part2/chapter06/program-6-8.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int
+bissexto(int ano) {
+    return (ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0;
+}
+
+int
+dias_do_mes(int mes, int ano) {
+    switch(mes) {
+        case 2:
+            if (bissexto(ano)) {
+                return 29;
+            }
+            return 28;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        default:
+            return 30;
+    }
+}
+
+int
+main(void) {
+    int ano, mes, dias;
+
+    printf("Indique o ano: ");
+    scanf(" %d", &ano);
+    printf("Indique o mes: ");
+    scanf(" %d", &mes);
+
+    dias = dias_do_mes(mes, ano);
+    printf("%d\n", dias);
+
+    return 0;
+}
